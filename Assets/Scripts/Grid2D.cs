8ecac8946e9368757df4a282468efe662d090a57
ktm-jg <raptarior@gmail.com>
2019-06-07 12:27:44 +0900

    Initial commit

---
+++ Assets/Scripts/Grid2D.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid2D : MonoBehaviour
+{
+    public GameObject nodePrefab;
+
+    public int nodeCount = 8;
+
+    void Awake()
+    {
+        nodePrefab = Resources.Load("Node") as GameObject;
+        CreateGrid(4);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void CreateGrid(int nodeCount)
+    {
+        float center = nodeCount / 2f;
+
+        for(int row = 0; row < nodeCount; row++)
+        {
+            for (int col = 0; col < nodeCount; col++)
+            {
+                GameObject node = Instantiate(nodePrefab, new Vector3(col + 0.5f - center, row + 0.5f - center, 0), Quaternion.identity);
+                Renderer renderer = node.GetComponent<Renderer>();
+                if (renderer != null) {
+                    Vector3 color = Random.insideUnitSphere;
+                    renderer.material.color = new Color(color.x, color.y, color.z);
+                }
+            }
+        }        
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    float gCost;
+    float hCost;
+    float fCost;
+    public float GCost
+    {
+        get { return gCost; }
+    }
+    public float HCost
+    {
+        get { return hCost; }
+    }
+    public float FCost
+    {
+        get { return gCost + hCost; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetGCost(float cost)
+    {
+        gCost = cost;
+    }
+
+    public void SetHCost(float cost)
+    {
+        hCost = cost;
+    }
+}
